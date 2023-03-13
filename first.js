21353fdf185f31f3232f47f8c120e4241085fbc8
Sérgio Filho <projectblackout150@gmail.com>
2023-03-13 16:21:26 -0300

    Nesta aula aprendemos um pouco mais sobre variaveis e quais seus tipos existentes

--- first.js
+++ first.js
@@ -1,6 +1,16 @@
 
-let nome = "Sergio"
-let idade = 90
+let nome = "Sergio A. Barbosa Filho" //string
+let idade = 90; //number
+let cidade = "Votuporanga"
+let estado = "SP"
+let logado = true // true=on false=off
+let lista = ["ovo", "massa" , "corante"] //array
+
+
+
+
 
 console.log(nome)
 console.log(idade)
+console.log(cidade)
+console.log(estado)
