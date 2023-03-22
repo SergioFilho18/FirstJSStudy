b01f9a55cf96825c50f9d4160cf711abc5dd2047
Sérgio Filho <projectblackout150@gmail.com>
2023-03-22 18:41:06 -0300

    Hoje conseguimos treinar bastante funções, usamos elementos de repetição como if e switch, e fizemos alguns exercícios.

--- first.js
+++ first.js
@@ -1,6 +1,20 @@
-function nameComplet(name, secondName) {
-    return `${name} ${secondName}`;
+function correctUser(user, password) {
+    if(user === 'Sérgio' && password === '123') {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-let complet = nameComplet("Sérgio", "Filho");
-console.log('Complet: ' + complet);
+let user = 'Sérgio';
+let password = '123';
+
+let correctly = correctUser(user, password);
+
+if(correctly) {
+    console.log('Accepted Access.');
+}
+else {
+    console.log('Access Denied.')
+}
