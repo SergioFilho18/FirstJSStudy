2424faf5ae11cd34582e88106c954fde4e120f55
Sérgio Filho <projectblackout150@gmail.com>
2023-04-04 08:56:34 -0300

    Na primeira 1 hora de aula hoje, ja conseguimos entender e aprender um pouco oque é array, no caso é uma variavel capaz de armazenar diversas informações, lembrou de alguma coisa?? sim tem a mesma função do vetor aprendido em C, porem mil vezes mais facil

--- first.js
+++ first.js
@@ -1,9 +1,27 @@
-function addSquares(a, b){
-    function square(x){
-     return x * x;
-    }
+//1. No array abaixo, qual o número que pega a Ferrari?
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1;
 
-    return square(a) + square(b);
-}
+console.log('1. ' + carros[x]);
 
-console.log(addSquares(2, 3));
+//2.Troque a Ferrari por Audi
+
+carros.pop();
+carros.pop();
+carros.push('Audi');
+carros.push('Mercedes');
+//ou, forma mais simples e correta seria:
+carros[1]= 'Audi';
+//
+console.log('2. Lista com Audi: ');
+console.log(carros);
+
+//3. Adicione o Volvo a Lista
+
+carros.push('Volvo');
+console.log('3. Lista com Volvo: ');
+console.log(carros);
+
+//4. Exiba quantos itens tem no array
+
+console.log(`4. Itens no array = ${carros.length}`);
