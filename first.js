763301db23ad1787a1f41d35f902194d70e7e18e
Sérgio Filho <projectblackout150@gmail.com>
2023-04-04 09:44:18 -0300

    Na 2 aula de hoje, aprendemos um pouco melhor de como funcionam os objetos, como manipula-los e utiliza-los

--- first.js
+++ first.js
@@ -1,27 +1,10 @@
-//1. No array abaixo, qual o número que pega a Ferrari?
-let carros = ['BMW', 'Ferrari', 'Mercedes'];
-let x = 1;
+let pessoa = {
+    nome: 'Bonieky',
+    sobrenome: 'Lacerda',
+    idade: 90,
+    nomeCompleto: function() {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+}
 
-console.log('1. ' + carros[x]);
-
-//2.Troque a Ferrari por Audi
-
-carros.pop();
-carros.pop();
-carros.push('Audi');
-carros.push('Mercedes');
-//ou, forma mais simples e correta seria:
-carros[1]= 'Audi';
-//
-console.log('2. Lista com Audi: ');
-console.log(carros);
-
-//3. Adicione o Volvo a Lista
-
-carros.push('Volvo');
-console.log('3. Lista com Volvo: ');
-console.log(carros);
-
-//4. Exiba quantos itens tem no array
-
-console.log(`4. Itens no array = ${carros.length}`);
+console.log(pessoa.nomeCompleto());
